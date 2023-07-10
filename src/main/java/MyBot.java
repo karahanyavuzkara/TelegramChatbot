6955a9e5106f56e76b83211a67b1e54ae3492324
nefretac <karahanyavuz61@gmail.com>
2023-07-10 21:00:13 +0300

    first

---
+++ src/main/java/MyBot.java
@@ -0,0 +1,64 @@
+import com.github.kshashov.telegram.api.MessageType;
+import com.github.kshashov.telegram.api.TelegramMvcController;
+import com.github.kshashov.telegram.api.TelegramRequest;
+import com.github.kshashov.telegram.api.bind.annotation.BotController;
+import com.github.kshashov.telegram.api.bind.annotation.BotPathVariable;
+import com.github.kshashov.telegram.api.bind.annotation.BotRequest;
+import com.github.kshashov.telegram.api.bind.annotation.request.MessageRequest;
+import com.pengrad.telegrambot.Callback;
+import com.pengrad.telegrambot.model.Chat;
+import com.pengrad.telegrambot.model.User;
+import com.pengrad.telegrambot.request.BaseRequest;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.BaseResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+public class MyBot {
+    @BotController
+    @SpringBootApplication
+    public class myBot implements TelegramMvcController {
+
+        @Value("${bot.token}")
+        private String token;
+
+        @Override
+        public String getToken() {
+            return token;
+        }
+
+        @BotRequest(value = "/hello", type = {MessageType.CALLBACK_QUERY, MessageType.MESSAGE})
+        public BaseRequest hello(User user, Chat chat) {
+            return new SendMessage(chat.id(), "Hello, " + user.firstName() + "!");
+        }
+
+        @MessageRequest("/hello {name:[\\S]+}")
+        public String helloWithName(@BotPathVariable("name") String userName) {
+            // Return a string if you need to reply with a simple message
+            return "Hello, " + userName;
+        }
+
+        @MessageRequest("/helloCallback")
+        public String helloWithCustomCallback(TelegramRequest request, User user) {
+            request.setCallback(new Callback() {
+                @Override
+                public void onResponse(BaseRequest request, BaseResponse response) {
+                    // TODO
+                }
+
+                @Override
+                public void onFailure(BaseRequest request, IOException e) {
+                    // TODO
+                }
+            });
+            return "Hello, " + user.firstName() + "!";
+        }
+
+        public static void main(String[] args) {
+            SpringApplication.run(myBot.class);
+        }
+    }
+}
--- src/main/java/simpleBot.java
+++ src/main/java/simpleBot.java
@@ -1,7 +1,10 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.Serializable;
 
 public class simpleBot extends TelegramLongPollingBot {
     @Override
@@ -10,6 +13,19 @@ public class simpleBot extends TelegramLongPollingBot {
         System.out.println(update.getMessage().getFrom().getFirstName());
 
         String command = update.getMessage().getText();
+        String start = update.toString();
+
+        String message0 = "Here is a command list for you !" +
+                           "/n"+
+                          "/info : To get further information about Zodiacbot."+
+                           "/n"+
+                          "/run : It is just for testing the bot with a joke."+
+                           "/n"+
+                          "/convo : It is for starting a conversation.";
+       
+        SendMessage startmsg = new SendMessage();
+        startmsg.setChatId(update.toString());
+        startmsg.setText(message0);
 
         if (command.equals("/run")) {
             String message = "Run, Forest Run !";
@@ -21,7 +37,38 @@ public class simpleBot extends TelegramLongPollingBot {
             try {
                 execute(response);
 
-            }catch (TelegramApiException E) {
+            } catch (TelegramApiException E) {
+                E.printStackTrace();
+            }
+        }
+
+        if (command.equals("/convo")) {
+            String message2 = "Hi, I am Zodiac. How can I help you ?";
+            SendMessage response2 = new SendMessage();
+            response2.setChatId(update.getMessage().getChatId().toString());
+            response2.setText(message2);
+
+
+            try {
+                execute(response2);
+
+            } catch (TelegramApiException E) {
+                E.printStackTrace();
+            }
+        }
+
+        if (command.equals("/info")) {
+            String message3 = "Hi, My name is Zodiac. My dad is Botfather and I am a Java coded Chatbot. I am still an " +
+                    "unfinished project. I would love to help you!";
+            SendMessage response3 = new SendMessage();
+            response3.setChatId(update.getMessage().getChatId().toString());
+            response3.setText(message3);
+
+
+            try {
+                execute(response3);
+
+            } catch (TelegramApiException E) {
                 E.printStackTrace();
             }
         }
